638afe2bbe3a044526efaeb5af7b2e55f8993fa6
jatinvyas1 <48468154+jatinvyas1@users.noreply.github.com>
2022-08-08 23:56:32 +0530

    removed unsed variable in app.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Home from "./Pages/index";
 import { BrowserRouter as Router } from "react-router-dom";
 
 function App() {
-  let myNewChange = "This is my Change";
   return (
     <Router>
       <Home />
